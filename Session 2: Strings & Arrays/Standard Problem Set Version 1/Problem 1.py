a58e1711d53d5bb1a6da882df49c8797bf29664e
Letitia09 <74643959+Letitia09@users.noreply.github.com>
2024-09-27 14:06:23 -0400

    Create Problem 1.py

---
+++ Session 2: Strings & Arrays/Standard Problem Set Version 1/Problem 1.py
@@ -0,0 +1,8 @@
+def reverse_sentence(sentence):
+    l = sentence.split() [::-1]
+    print(" ".join(l))
+sentence = "tubby little cubby all stuffed with fluff"
+reverse_sentence(sentence)
+
+sentence = "Pooh"
+reverse_sentence(sentence)
