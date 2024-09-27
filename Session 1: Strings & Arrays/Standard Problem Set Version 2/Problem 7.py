7973dd76abc7de314275056f528aba052fd51b54
Letitia09 <74643959+Letitia09@users.noreply.github.com>
2024-09-27 10:48:29 -0400

    Create Problem 7.py

---
+++ Session 1: Strings & Arrays/Standard Problem Set Version 2/Problem 7.py
@@ -0,0 +1,14 @@
+def nanana_batman(x):
+	if x==0 :
+		print("batman!")
+	else:
+		sum = ""
+		for i in range(x):
+			sum += "na"
+		print(sum+" batman!")
+		
+x = 6
+nanana_batman(x)
+
+x = 0
+nanana_batman(x)
