bd244aa02419d6fc28cfdc7bc56cbc4de0a63dbe
Letitia09 <74643959+Letitia09@users.noreply.github.com>
2024-09-27 11:53:32 -0400

    Create Problem 1.py

---
+++ Session 1: Strings & Arrays/Advanced Problem Set Version 1/Problem 1.py
@@ -0,0 +1,15 @@
+def linear_search(lst, target):
+	for i in range(len(lst)):
+		if lst[i] == target:
+			print(i)
+			break
+	else:
+		print(-1)
+			
+items = ['haycorn', 'haycorn', 'haycorn', 'hunny', 'haycorn']
+target = 'hunny'
+linear_search(items, target)
+
+items = ['bed', 'blue jacket', 'red shirt', 'hunny']
+target = 'red balloon'
+linear_search(items, target)
