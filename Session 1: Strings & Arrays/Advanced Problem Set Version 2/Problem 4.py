8f64df4b6336626f8a4d3107b4daae7422850243
Letitia09 <74643959+Letitia09@users.noreply.github.com>
2024-10-04 23:48:19 -0400

    Create Problem 4.py

---
+++ Session 1: Strings & Arrays/Advanced Problem Set Version 2/Problem 4.py
@@ -0,0 +1,16 @@
+def minimum_boxes(meals, capacity):
+	sum_meals = sum(meals)
+	box_count=0
+	for i in capacity:
+	    sum_meals -= i
+	    box_count+=1
+	    if sum_meals<0:
+	        break
+	print(box_count)
+meals = [1, 3, 2]
+capacity = [4, 3, 1, 5, 2]
+minimum_boxes(meals, capacity)
+
+meals = [5, 5, 5]
+capacity = [2, 4, 2, 7]
+minimum_boxes(meals, capacity)
