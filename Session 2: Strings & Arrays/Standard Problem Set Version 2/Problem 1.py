0bb98c60041dec40bea77d408cf958add1d3bbc6
Letitia09 <74643959+Letitia09@users.noreply.github.com>
2024-10-05 18:57:40 -0400

    Update Problem 1.py

--- Session 2: Strings & Arrays/Standard Problem Set Version 2/Problem 1.py
+++ Session 2: Strings & Arrays/Standard Problem Set Version 2/Problem 1.py
@@ -1,1 +1,18 @@
+def are_equivalent(word1, word2):
+    word1 = "".join(word1)
+    word2 = "".join(word2)
+    if word1 == word2:
+        print(True)
+    else:
+        print(False)
+word1 = ["bat", "man"]
+word2 = ["b", "atman"]
+are_equivalent(word1, word2)
 
+word1 = ["alfred", "pennyworth"]
+word2 = ["alfredpenny", "word"]
+are_equivalent(word1, word2)
+
+word1  = ["cat", "wom", "an"]
+word2 = ["catwoman"]
+are_equivalent(word1, word2)
