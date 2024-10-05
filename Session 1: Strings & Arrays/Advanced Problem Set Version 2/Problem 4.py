c24934925d333d32d2f8ab1ef4bdb6cf5047724f
Letitia09 <74643959+Letitia09@users.noreply.github.com>
2024-10-04 23:49:48 -0400

    Update Problem 4.py

--- Session 1: Strings & Arrays/Advanced Problem Set Version 2/Problem 4.py
+++ Session 1: Strings & Arrays/Advanced Problem Set Version 2/Problem 4.py
@@ -1,10 +1,11 @@
 def minimum_boxes(meals, capacity):
 	sum_meals = sum(meals)
+	capacity.sort(reverse=True)
 	box_count=0
 	for i in capacity:
 	    sum_meals -= i
 	    box_count+=1
-	    if sum_meals<0:
+	    if sum_meals<=0:
 	        break
 	print(box_count)
 meals = [1, 3, 2]
