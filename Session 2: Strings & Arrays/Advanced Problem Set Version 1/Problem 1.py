a7311dc9a46f83af1f736ce4cac8c5df5b8dc604
Letitia09 <74643959+Letitia09@users.noreply.github.com>
2024-10-08 12:08:45 -0400

    Create Problem 1.py

---
+++ Session 2: Strings & Arrays/Advanced Problem Set Version 1/Problem 1.py
@@ -0,0 +1,26 @@
+def transpose(matrix):
+    if not matrix or not matrix[0]:
+        return []
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+    
+    transposed_matrix = [[0] * rows for _ in range(cols)]
+    
+    for i in range(rows):
+        for j in range(cols):
+            transposed_matrix[j][i] = matrix[i][j]
+    
+    print(transposed_matrix)
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+transpose(matrix)
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+transpose(matrix)
