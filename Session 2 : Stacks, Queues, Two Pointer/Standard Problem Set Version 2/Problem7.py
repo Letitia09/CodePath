ee99a359a6930d89c9b547f4f5d90dee58b506e9
Letitia09 <74643959+Letitia09@users.noreply.github.com>
2024-11-24 08:46:33 -0500

    Update and rename Problem 7: Check if a Signal Occurs as a Prefix in Any Transmission to Problem7.py

---
+++ Session 2 : Stacks, Queues, Two Pointer/Standard Problem Set Version 2/Problem7.py
@@ -0,0 +1,18 @@
+
+#Problem 7: Check if a Signal Occurs as a Prefix in Any Transmission. py
+
+def is_prefix_of_signal(transmission, searchSignal):
+    str_list = transmission.split()
+    count, flag = 0, 0
+    n = len(searchSignal)-1
+    for i in str_list:
+        count+=1
+        if searchSignal in i and searchSignal[n] == i[n] and searchSignal[0] == i[0]:
+            flag = 1
+            return count
+    if flag == 0 :
+        return -1
+     
+print(is_prefix_of_signal("hellohello hellohellohello", "ell")) 
+print(is_prefix_of_signal("this problem is an easy problem", "pro")) 
+print(is_prefix_of_signal("i am tired", "you")) 
